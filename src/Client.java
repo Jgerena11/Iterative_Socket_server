266fb021d5265f4e1ce6bb79693be353a994714a
Jose <jgerena1111@gmail.com>
2020-02-27 19:45:05 -0500

    changes made to client and server

--- src/Client.java
+++ src/Client.java
@@ -1,61 +1,148 @@
 import java.net.*;
-import java.util.*;
+import java.time.LocalDate;
 import java.io.*;
-import java.util.logging.*;
+import java.util.*;
 import java.lang.*;
 
 public class Client extends Thread{
-	private static Socket sock;
+	private Socket sock;
 	private BufferedReader readIn;
 	private DataOutputStream sendOut;
 	private static String address;
 	private static int port;
+	private static int threadCount;
 	private static String command = "manual";
-	private static long totalFinalTime = 0;
+	private static double totalFinalTime = 0;
 	
-	public Client(String address, int port, String s) {
-		Client.address = address;
-		Client.port = port;
-		Client.command = s; 
+	public Client(String add, int pot, String comm) throws UnknownHostException, IOException {
+		address = add;
+		port = pot;
+		command = comm;
+	}
+	public void run() {
+		
+		
+		try {
+			Socket m = new Socket(address, port);
+			Scanner B = new Scanner(m.getInputStream());
+			PrintStream g = new PrintStream(m.getOutputStream());
+			g.println(command);
+			double start = System.currentTimeMillis();
+			System.out.println(command);
+			if (B.hasNext()) {
+				double end = System.currentTimeMillis();
+				double elapsedTime = end - start;
+				totalFinalTime += elapsedTime;
+			}
+			if(command == "1") {
+				System.out.println("command 1 inputed");
+				System.out.println(B.next());
+			}else {
+				while(B.hasNext() && B.next() !=null) {	
+					System.out.println(B.nextLine());
+				}
+			
+				g.flush();
+				B.close();
+				m.close();
+		
+			}
+		}catch(IOException ee) {}
+	
+	
 	}
 	
-	public static void main(String[] args) throws UnknownHostException, IOException {
-		Scanner sc1 = new Scanner(sock.getInputStream());
-		Scanner scanner = new Scanner(System.in);
+	public static void main(String args[]) throws UnknownHostException, IOException  {
+		String obj;
 		
-		System.out.println("Enter Hostname: ");
-		address = scanner.next(args[0]);
+		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter Address: ");
+//		
+//		address = sc.next();
+//		System.out.println("Enter Port number: ");
+//		port = sc.nextInt();
 		
-		System.out.println("Enter Port: ");
-		port = Integer.parseInt(args[1]);
+		address = "139.62.210.153";
+		port = 1236;
 		
-		int threadCount;
-		int check = -1; 
-		
-		Client thread; 
-
-		try { 
+		int check = -1;
+		Client testConnection[];
+		testConnection = new Client[1];
+		Client threads[];
+		threads = new Client[1002];
+		try{
 			System.out.println("Connecting to Server");
-			thread = new Client(address, port, "manual");
-			thread.start();
-			thread.join();
-			System.out.println("Now connected to Server"); 	
-		}catch(Exception e) {
-			System.out.print("connection failed");
-			e.printStackTrace(); 
+			threads[1000] = new Client(address, port, "manual");
+			//threads[1000].start();
+			threads[1000].run();
+			threads[1000].join();
+			System.out.println("Now connected to Server");}
+		
+		catch(InterruptedException interupt){
+			System.out.println("Connection Failed");
 			return;
 		}
 		
-		while(!command.equals("quit")) {
+		while (!command.equals("quit")){
 			
-			System.out.println("Please Enter command, or manual for options or quit");
-			command = scanner.next();
-			if (command.contentEquals("quit")){
+			System.out.println("How many Jawns do you wanna spawn?");
+			threadCount = sc.nextInt();
+	
+			do {
+				try {
+					if(check!=0){
+						System.out.println("please enter a number 1 or greater");
+						check++;
+					}
+				}catch(InputMismatchException mm){
+					System.out.println("Please enter a valid number");
+					threadCount = -1;
+					sc.next();
+				}
+			}
+		
+			while(threadCount<1); 
+
+		
+			System.out.println("Please Enter command, or manual for options, or quit:");
+			command = sc.next();
+			if(command.equals("quit")){
 				System.out.println("Ending Connection");
 				System.exit(0);
 			}
+			
+			
+			for(int i = 0; i<threadCount; i++) {
+				threads[i] = new Client(address, port, command);
+			}
+
+			System.out.print("Running " + threadCount + " threads for command: " + command);
+
+			for (int i = 0; i < threadCount; i++){
+				threads[i].run();
+			}
+		
+
+			for (int i = 0; i < threadCount; i++){
+				try{
+					threads[i].join();
+				}catch (InterruptedException interupt){
+				}
+			}
+
+
+			System.out.println("\n ***Thread: " + threadCount + ";command: " + command +";Average time in milliseconds: " + totalFinalTime/threadCount + "***\n\n");
+			System.out.print("Thread: " + threadCount + ";command: " + command +";Average time in milliseconds: " + totalFinalTime/threadCount+"\n");
+			totalFinalTime = 0;
+
+			for(int i = 0; i < threadCount; i++) 
+				threads[i] = null;
+
+
 		}
+		sc.close();
+	
 	}
+	
 
 }
-
--- src/SocketServer.java
+++ src/SocketServer.java
@@ -5,53 +5,122 @@ import java.util.*;
  import java.lang.*;
 
 public class SocketServer {
- 
-  public static void main(String[] args) throws IOException {
-	int number, temp;
-	  
-	  ServerSocket s1 = new ServerSocket(1342);
-	
-	Socket ss = s1.accept();
-	Scanner sc = new Scanner(ss.getInputStream());
-	number = sc.nextInt();
-	PrintStream p = new PrintStream(ss.getOutputStream());
-	
-	switch(number) {
-	
-	case 1:
-		  LocalDate myObj = LocalDate.now();
-			p.println(myObj);
-	break;
-	case 2:
-		Process l = Runtime.getRuntime().exec("uptime");
-		BufferedReader b = new BufferedReader(new InputStreamReader(l.getInputStream()));
-		
-		p.print(b.readLine());
-	break; 
-	case 3:
-		Process bb = Runtime.getRuntime().exec("free");
-		
-		BufferedReader v = new BufferedReader(new InputStreamReader(bb.getInputStream()));
-		
-		String el = null;
-		while((el = v.readLine()) != null) {
-			p.print(el);
-			p.println();
-			
-			
-		}
-		
-		
-		break;
-	default:
-		
-	p.println("no luck");
 
-  
+ public static PrintStream p;
+ public static String choice;
+
+  public static void main(String[] args) throws IOException {
+	int  temp;
+	 String choice;
+	 
 	
+	 
+	  ServerSocket s1 = new ServerSocket(1236);
+while(true) {
+	try {
+		Socket ss = s1.accept();
+		System.out.println("New connection");
+		Scanner sc = new Scanner(ss.getInputStream());
+		p = new PrintStream(ss.getOutputStream());
+		
+		
+		choice = sc.next();
+			
+			switch(choice) {
+			case "1":
+			  LocalDate myObj = LocalDate.now();
+			  System.out.println(myObj);
+			  p.println(myObj);
+				
+				p.flush();
+				break;
+			case "2":
+				Process l = Runtime.getRuntime().exec("uptime");
+				BufferedReader b = new BufferedReader(new InputStreamReader(l.getInputStream()));
+				p.print(b.readLine());
+				p.flush();
+				break; 
+			case "3":
+				Process bb = Runtime.getRuntime().exec("free");
+			
+				BufferedReader v = new BufferedReader(new InputStreamReader(bb.getInputStream()));
+			
+				String el = null;
+				while((el = v.readLine()) != null) {
+					p.print(el);
+					p.println();	
+				}
+				p.flush();
+				break; 
+			case "4":
+			
+				Process ee = Runtime.getRuntime().exec("netstat");
+				BufferedReader g = new BufferedReader(new InputStreamReader(ee.getInputStream()));
+				String pp = null;
+				while((pp = g.readLine()) != null) {
+					p.print(pp);
+					p.println();
+				}
+				
+			p.flush();
+				break;
+			
+			case "5":
+				Process wd = Runtime.getRuntime().exec("w");
+				BufferedReader h = new BufferedReader(new InputStreamReader(wd.getInputStream()));
+				String lm = null;
+				while((lm = h.readLine()) != null) {
+					p.println();
+					p.print(lm);
+					
+				}
+				p.flush();
+				break;
+		
+			case "6":
+				Process yy = Runtime.getRuntime().exec("ps -ef");
+				BufferedReader y = new BufferedReader(new InputStreamReader(yy.getInputStream()));
+				String hw = null;
+				while((hw = y.readLine()) != null) {
+					p.println();
+					p.print(hw);
+					
+					}
+				System.out.println("End");
+				p.flush();
+				
+				break;
+				
+			case "manual":
+				p.println("Please make a selection\n Press 1 for date and time\n");
+				
+				p.println("Press 2 for uptime ");
+				p.println("Press 3 for memory use");
+				p.println("Press 4 for NetStat");
+				p.println("Press 5 for current users");
+				p.println("press 6 for Running Processes");
+				
+				p.flush();
+				break;
+			
+			default:
+				p.println("no luck");
+
+			p.flush();
+			
+			}
+			System.out.println("End");
+			ss.close();
+			sc.close();
+			p.close();
+			
+			
+			
+	}catch(IOException e) {
+		p.flush();
+		e.getStackTrace();
+	}	
+}
 	}
-	
-	
-	
-  
-}}
+  }
+ 
